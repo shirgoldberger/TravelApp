a98dc6709b1c139319759453e86c53b923b1b7f3
shirgoldberger <shirgoldberger@gmail.com>
2020-11-05 15:58:13 +0200

    home page

---
+++ TravelApp/HomePage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TravelApp
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : INotifyPropertyChanged
+    {
+        private string username = "";
+        private string password = "";
+        private string message = "";
+        public HomePage()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        private void Button_Click_Login(object sender, RoutedEventArgs e)
+        {
+            Message = "";
+            // if the user didn't insert username or password
+            if (username.Equals("") || password.Equals(""))
+            {
+                Message = "You need to insert username and password";
+                return;
+            }
+            // Check the username & password 
+        }
+        private void Button_Click_New_Account(object sender, RoutedEventArgs e)
+        {
+            CreateAccount ca = new CreateAccount();
+            this.NavigationService.Navigate(ca);
+        }
+        public string Username
+        {
+            get
+            {
+                return this.username;
+            }
+            set
+            {
+                this.username = value;
+                NotifyPropertyChanged("Ip");
+
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                this.password = value;
+                NotifyPropertyChanged("Port");
+            }
+        }
+        public string Message
+        {
+            get { return this.message; }
+            set
+            {
+                this.message = value;
+                NotifyPropertyChanged("Message");
+            }
+        }
+
+
+        private void Button_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            // close the program
+            System.Environment.Exit(0);
+        }
+
+        private void Username_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Message = "";
+        }
+
+        private void Password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Message = "";
+        }
+    }
+}
--- TravelApp/MainWindow.xaml.cs
+++ TravelApp/MainWindow.xaml.cs
@@ -12,17 +12,21 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace TravelApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            ShowsNavigationUI = false;
         }
     }
 }
