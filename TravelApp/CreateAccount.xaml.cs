661b50f757eb2c99ee03721540a78407cbf73be5
giladby <56963755+giladby@users.noreply.github.com>
2020-11-10 01:33:33 +0200

    update some xamls

--- TravelApp/CreateAccount.xaml.cs
+++ TravelApp/CreateAccount.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -21,32 +24,117 @@ namespace TravelApp
     /// </summary>
     public partial class CreateAccount : Page
     {
-        // male = 0, female = 1
-        private int gender;
+        private List<MyLanguage> objLanguagesList;
+        private List<string> languages;
+        private List<string> choosenLanguages;
+        public class MyLanguage
+        {
+            public int LanguageID
+            {
+                get;
+                set;
+            }
+            public string LanguageString
+            {
+                get;
+                set;
+            }
+            public Boolean Check_Status
+            {
+                get;
+                set;
+            }
+        }
         public CreateAccount()
         {
             InitializeComponent();
+            choosenLanguages = new List<string>();
+            languages = new List<string>(){ "Hebrew", "English", "French", "Arabic", "Spanish" };
+            objLanguagesList = InsertElementsInList(languages);
+            BindLanguagesDropDown();
         }
-        private void button2_Click(object sender, RoutedEventArgs e)
+
+        private List<MyLanguage> InsertElementsInList(List<string> languages)
+        {
+            List<MyLanguage> returnedList = new List<MyLanguage>();
+            int counter = 0;
+            foreach (string language in languages)
+            {
+                MyLanguage lang = new MyLanguage();
+                lang.LanguageID = counter++;
+                lang.LanguageString = language;
+                returnedList.Add(lang);
+            }
+            return returnedList;
+        }
+
+        private void MyCheckedAndUnchecked(object sender, RoutedEventArgs e)
+        {
+            var baseobj = sender as FrameworkElement;
+            var language = baseobj.DataContext as MyLanguage;
+            BindListBox(language);
+        }
+
+        private void resetLanguages()
+        {
+            choosenLanguages.Clear();
+            foreach (var language in objLanguagesList)
+            {
+                language.Check_Status = false;
+            }
+            languagesComboBox.SelectedIndex = -1;
+        }
+        private void BindListBox(MyLanguage language)  
+        {
+            if(language.Check_Status)
+            {
+                choosenLanguages.Add(language.LanguageString);
+            }
+            else
+            {
+                choosenLanguages.Remove(language.LanguageString);
+            }
+        }
+
+        private void BindLanguagesDropDown()
+        {
+            languagesComboBox.ItemsSource = objLanguagesList;
+        }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
         {
             Reset();
         }
         public void Reset()
         {
-            //textBoxFirstName.Text = "";
-            //textBoxLastName.Text = "";
+            textBoxUserName.Text = "";
+            textBoxPhone.Text = "";
+            textBoxAge.Text = "";
             textBoxEmail.Text = "";
             textBoxAddress.Text = "";
-            passwordBox1.Password = "";
+            passwordBox.Password = "";
             passwordBoxConfirm.Password = "";
+            male.IsChecked = false;
+            female.IsChecked = false;
+            resetLanguages();
         }
-        private void button3_Click(object sender, RoutedEventArgs e)
+        private void cancel_Click(object sender, RoutedEventArgs e)
         {
             HomePage login = new HomePage();
             this.NavigationService.Navigate(login);
         }
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
+            if (choosenLanguages.Count == 0)
+            {
+                errormessage.Text = "Please choose languages";
+                languagesComboBox.Focus();
+            }
+            if (male.IsChecked == false && female.IsChecked == false)
+            {
+                errormessage.Text = "Choose Gender";
+                male.Focus();
+            }
             if (textBoxEmail.Text.Length == 0)
             {
                 errormessage.Text = "Enter an email.";
@@ -63,19 +151,29 @@ namespace TravelApp
                 string firstname = textBoxUserName.Text;
                 string lastname = textBoxPhone.Text;
                 string email = textBoxEmail.Text;
-                string password = passwordBox1.Password;
-                int age = int.Parse(textBoxAge.Text);
-                if (passwordBox1.Password.Length == 0)
+                string password = passwordBox.Password;
+                int age;
+                try
+                {
+                    age = int.Parse(textBoxAge.Text);
+                }
+                catch
+                {
+                    errormessage.Text = "Enter valid age";
+                    textBoxAge.Focus();
+                }
+                
+                if (passwordBox.Password.Length == 0)
                 {
                     errormessage.Text = "Enter password.";
-                    passwordBox1.Focus();
+                    passwordBox.Focus();
                 }
                 else if (passwordBoxConfirm.Password.Length == 0)
                 {
                     errormessage.Text = "Enter Confirm password.";
                     passwordBoxConfirm.Focus();
                 }
-                else if (passwordBox1.Password != passwordBoxConfirm.Password)
+                else if (passwordBox.Password != passwordBoxConfirm.Password)
                 {
                     errormessage.Text = "Confirm password must be same as password.";
                     passwordBoxConfirm.Focus();
@@ -95,16 +193,25 @@ namespace TravelApp
                 }
             }
         }
-        private void CheckBox_Checked2(object sender, RoutedEventArgs e)
+        private void male_Checked(object sender, RoutedEventArgs e)
         {
-            gender = 0;
             female.IsChecked = false;
         }
-        private void CheckBox_Checked1(object sender, RoutedEventArgs e)
+        private void female_Checked(object sender, RoutedEventArgs e)
         {
-            gender = 1;
             male.IsChecked = false;
         }
+
+        private void languagesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Languages_TextChanged(object sender, EventArgs e)
+        {
+            languagesComboBox.ItemsSource = objLanguagesList.Where(x => x.LanguageString.StartsWith(languagesComboBox.Text.Trim()));
+        }
+
         private void Button_Click_Exit(object sender, RoutedEventArgs e)
         {
 
--- TravelApp/addNewTrip.xaml.cs
+++ TravelApp/addNewTrip.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+enum Type
+{
+    Friends, Properties, Locations
+}
 namespace TravelApp
 {
     /// <summary>
@@ -21,93 +26,322 @@ namespace TravelApp
     /// </summary>
     public partial class addNewTrip : Page
     {
-        private int gender;
+        
+        private ArrayList objLocationList;
+        private ArrayList objPropertiesList;
+        private ArrayList objFriendsList;
+        private List<string> friends;
+        private List<string> locations;
+        private List<string> properties;
+        private List<string> choosenLocations;
+        private List<string> choosenProperties;
+        private List<string> choosenFriends;
+        public class MyLocation
+        {
+            public int LocationID
+            {
+                get;
+                set;
+            }
+            public string LocationString
+            {
+                get;
+                set;
+            }
+            public Boolean Check_Status
+            {
+                get;
+                set;
+            }
+        }
 
+        public class MyFriend
+        {
+            public int FriendID
+            {
+                get;
+                set;
+            }
+            public string FriendString
+            {
+                get;
+                set;
+            }
+            public Boolean Check_Status
+            {
+                get;
+                set;
+            }
+        }
+
+        public class MyProperty
+        {
+            public int PropID
+            {
+                get;
+                set;
+            }
+            public string ProprtyString
+            {
+                get;
+                set;
+            }
+            public Boolean Check_Status
+            {
+                get;
+                set;
+            }
+        }
         public addNewTrip()
         {
             InitializeComponent();
+            choosenLocations = new List<string>();
+            choosenProperties = new List<string>();
+            choosenFriends = new List<string>();
+            friends = new List<string>() { "Gilad", "Ben", "Yael", "Rinat", "Ehud" };
+            locations = new List<string>() { "Paris", "Eilat", "Roma", "London", "Haifa" };
+            properties = new List<string>() { "Adventurous", "Organized", "Cultural" };
+            objLocationList = InsertElementsInList(locations, Type.Locations);
+            objFriendsList = InsertElementsInList(friends, Type.Friends);
+            objPropertiesList = InsertElementsInList(properties, Type.Properties);
+            BindCBDropDown<MyProperty>(propComboBox, objPropertiesList);
+            BindCBDropDown<MyLocation>(locationComboBox, objLocationList);
+            BindCBDropDown<MyFriend>(friendsComboBox, objFriendsList);
         }
-        private void button2_Click(object sender, RoutedEventArgs e)
+
+        private ArrayList InsertElementsInList(List<string> inputList, Type type)
+        {
+            ArrayList returnedList = new ArrayList();
+            int counter = 0;
+            foreach (string element in inputList)
+            {
+                switch(type)
+                {
+                    case Type.Friends:
+                        MyFriend friend = new MyFriend();
+                        friend.FriendID = counter++;
+                        friend.FriendString = element;
+                        returnedList.Add(friend);
+                        break;
+                    case Type.Locations:
+                        MyLocation location = new MyLocation();
+                        location.LocationID = counter++;
+                        location.LocationString = element;
+                        returnedList.Add(location);
+                        break;
+                    case Type.Properties:
+                        MyProperty prop = new MyProperty();
+                        prop.PropID = counter++;
+                        prop.ProprtyString = element;
+                        returnedList.Add(prop);
+                        break;
+                }
+            }
+            return returnedList;
+        }
+
+        private void BindCBDropDown<T>(ComboBox cb, ArrayList objList)
+        {
+            cb.ItemsSource = objList;
+        }
+
+        private void MyCheckedAndUncheckedProps(object sender, RoutedEventArgs e)
+        {
+            var baseobj = sender as FrameworkElement;
+            var prop = baseobj.DataContext as MyProperty;
+            BindListBox<MyProperty>(prop, choosenProperties, Type.Properties);
+        }
+
+        private void MyCheckedAndUncheckedLocations(object sender, RoutedEventArgs e)
+        {
+            var baseobj = sender as FrameworkElement;
+            var location = baseobj.DataContext as MyLocation;
+            BindListBox<MyLocation>(location, choosenLocations, Type.Locations);
+        }
+
+        private void MyCheckedAndUncheckedFriends(object sender, RoutedEventArgs e)
+        {
+            var baseobj = sender as FrameworkElement;
+            var friend = baseobj.DataContext as MyFriend;
+            BindListBox<MyFriend>(friend, choosenFriends, Type.Friends);
+        }
+
+        private void BindListBox<T>(T currObject, List<string> inputList, Type type)
+        {
+            switch (type)
+            {
+                case Type.Friends:
+                    var friend = currObject as MyFriend;
+                    if (friend.Check_Status)
+                    {
+                        inputList.Add(friend.FriendString);
+                    }
+                    else
+                    {
+                        inputList.Remove(friend.FriendString);
+                    }
+                    break;
+                case Type.Locations:
+                    var location = currObject as MyLocation;
+                    if (location.Check_Status)
+                    {
+                        inputList.Add(location.LocationString);
+                    }
+                    else
+                    {
+                        inputList.Remove(location.LocationString);
+                    }
+                    break;
+                case Type.Properties:
+                    var prop = currObject as MyProperty;
+                    if (prop.Check_Status)
+                    {
+                        inputList.Add(prop.ProprtyString);
+                    }
+                    else
+                    {
+                        inputList.Remove(prop.ProprtyString);
+                    }
+                    break;
+            }
+            
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e)
         {
             Reset();
         }
         public void Reset()
         {
-            //textBoxFirstName.Text = "";
-            //textBoxLastName.Text = "";
-            textBoxEmail.Text = "";
-            textBoxAddress.Text = "";
-            passwordBox1.Password = "";
-            passwordBoxConfirm.Password = "";
+            tripDates.SelectedDate = null;
+            tripDates.DisplayDate = DateTime.Today;
+            tripNameVal.Text = "";
+            minParticipantsVal.Text = "";
+            maxParticipantsVal.Text = "";
+            resetComboBox(friendsComboBox, choosenFriends, objFriendsList, Type.Friends);
+            resetComboBox(locationComboBox, choosenLocations, objLocationList, Type.Locations);
+            resetComboBox(propComboBox, choosenProperties, objPropertiesList, Type.Properties);
         }
-        private void button3_Click(object sender, RoutedEventArgs e)
+
+        private void resetComboBox(ComboBox cb, List<string> lst, ArrayList objList, Type type)
+        {
+            
+            switch (type)
+            {
+                case Type.Friends:
+                    
+                    foreach (var friend in objList)
+                    {
+                        var friendCasted = friend as MyFriend;
+                        friendCasted.Check_Status = false;
+                    }
+                    break;
+                case Type.Locations:
+                    foreach (var location in objList)
+                    {
+                        var locationCasted = location as MyLocation;
+                        locationCasted.Check_Status = false;
+                    }
+                    break;
+                case Type.Properties:
+                    foreach (var prop in objList)
+                    {
+                        var propCasted = prop as MyProperty;
+                        propCasted.Check_Status = false;
+                    }
+                    break;
+            }
+            lst.Clear();
+            cb.SelectedIndex = -1;
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
         {
             HomePage login = new HomePage();
             this.NavigationService.Navigate(login);
         }
+
+        private void Friend_TextChanged(object sender, EventArgs e)
+        {
+            friendsComboBox.ItemsSource = objFriendsList.Cast<MyFriend>().ToList().Where(x => x.FriendString.StartsWith(friendsComboBox.Text.Trim()));
+        }
+
+        private void Properties_TextChanged(object sender, EventArgs e)
+        {
+            propComboBox.ItemsSource = objPropertiesList.Cast<MyProperty>().ToList().Where(x => x.ProprtyString.StartsWith(propComboBox.Text.Trim()));
+        }
+
+        private void Location_TextChanged(object sender, EventArgs e)
+        {
+            locationComboBox.ItemsSource = objLocationList.Cast<MyLocation>().ToList().Where(x => x.LocationString.StartsWith(locationComboBox.Text.Trim()));
+        }
+
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
-            if (textBoxEmail.Text.Length == 0)
+            if (tripDates.SelectedDate == null)
             {
-                errormessage.Text = "Enter an email.";
-                textBoxEmail.Focus();
+                errormessage.Text = "Enter dates of the trip";
+                tripDates.Focus();
+                return;
             }
-            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            if (choosenLocations.Count == 0)
             {
-                errormessage.Text = "Enter a valid email.";
-                textBoxEmail.Select(0, textBoxEmail.Text.Length);
-                textBoxEmail.Focus();
+                errormessage.Text = "Enter location for the trip";
+                locationComboBox.Focus();
+                return;
+            }
+            if (choosenProperties.Count == 0)
+            {
+                errormessage.Text = "Enter properties of the trip";
+                propComboBox.Focus();
+                return;
+            }
+            if (minParticipantsVal.Text == "" || maxParticipantsVal.Text == "")
+            {
+                errormessage.Text = "Enter number of participants for the trip";
+                minParticipantsVal.Focus();
+                return;
             }
             else
             {
-                string firstname = textBoxUserName.Text;
-                string lastname = textBoxPhone.Text;
-                string email = textBoxEmail.Text;
-                string password = passwordBox1.Password;
-                int age = int.Parse(textBoxAge.Text);
-                if (passwordBox1.Password.Length == 0)
+                try
                 {
-                    errormessage.Text = "Enter password.";
-                    passwordBox1.Focus();
+                    int min = int.Parse(minParticipantsVal.Text);
+                    int max = int.Parse(maxParticipantsVal.Text);
+                    if(min < 1 || min > max)
+                    {
+                        errormessage.Text = "Enter valid number of participants for the trip";
+                        minParticipantsVal.Focus();
+                        return;
+                    }
                 }
-                else if (passwordBoxConfirm.Password.Length == 0)
+                catch
                 {
-                    errormessage.Text = "Enter Confirm password.";
-                    passwordBoxConfirm.Focus();
-                }
-                else if (passwordBox1.Password != passwordBoxConfirm.Password)
-                {
-                    errormessage.Text = "Confirm password must be same as password.";
-                    passwordBoxConfirm.Focus();
-                }
-                else
-                {
-                    //errormessage.Text = "";
-                    //string address = textBoxAddress.Text;
-                    //SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
-                    //con.Open();
-                    //SqlCommand cmd = new SqlCommand("Insert into Registration (FirstName,LastName,Email,Password,Address) values('" + firstname + "','" + lastname + "','" + email + "','" + password + "','" + address + "')", con);
-                    //cmd.CommandType = CommandType.Text;
-                    //cmd.ExecuteNonQuery();
-                    //con.Close();
-                    //errormessage.Text = "You have Registered successfully.";
-                    //Reset();
+                    errormessage.Text = "Enter valid number of participants for the trip";
+                    minParticipantsVal.Focus();
+                    return;
                 }
             }
-        }
-        private void CheckBox_Checked2(object sender, RoutedEventArgs e)
-        {
-            gender = 0;
-            female.IsChecked = false;
-        }
-        private void CheckBox_Checked1(object sender, RoutedEventArgs e)
-        {
-            gender = 1;
-            male.IsChecked = false;
+            if(tripNameVal.Text == "")
+            {
+                errormessage.Text = "Enter the name of the trip";
+                tripNameVal.Focus();
+                return;
+            }
         }
         private void Button_Click_Exit(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void maleChecked(object sender, RoutedEventArgs e)
+        {
+            female.IsChecked = false;
+        }
+
+        private void femaleChecked(object sender, RoutedEventArgs e)
+        {
+            male.IsChecked = false;
         }
     }
 }
